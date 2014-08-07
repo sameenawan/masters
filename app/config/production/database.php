8ce2e0c518d9fc9970101e2032a704c82224ae18
Sameen Awan <awan@g.harvard.edu>
2014-08-07 18:58:48 -0400

    project4 database

---
+++ app/config/production/database.php
@@ -0,0 +1,17 @@
+<?php
+
+return array(
+
+    'connections' => array(
+
+        'mysql' => array(
+            'driver'    => 'mysql',
+            'host'      => 'tunnel.pagodabox.com:3306',
+            'database'  => 'masters',
+            'username'  => 'jayme',
+            'password'  => 'J8mjxKSa',
+        ),
+
+    ),
+
+);
