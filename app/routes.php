18dbcf120528506759872571a1030420f3a16b59
Sameen Awan <awan@g.harvard.edu>
2014-08-08 11:26:06 -0400

    project4 code2

--- app/routes.php
+++ app/routes.php
@@ -1,6 +1,132 @@
 <?php
 
 
+
+
+<?php
+
+/*-------------------------------------------------------------------------------------------------
+// ! Index
+-------------------------------------------------------------------------------------------------*/
+Route::get('/', 'IndexController@getIndex');
+
+
+
+/*-------------------------------------------------------------------------------------------------
+// ! User
+Explicit Routing
+-------------------------------------------------------------------------------------------------*/
+# Note: the beforeFilter for 'guest' on getSignup and getLogin is handled in the Controller
+Route::get('/signup', 'UserController@getSignup'); 
+Route::get('/login', 'UserController@getLogin' );
+Route::post('/signup', ['before' => 'csrf', 'uses' => 'UserController@postSignup'] );
+Route::post('/login', ['before' => 'csrf', 'uses' => 'UserController@postLogin'] );
+Route::get('/logout', ['before' => 'auth', 'uses' => 'UserController@getLogout'] );
+
+
+/*-------------------------------------------------------------------------------------------------
+// ! Tag 
+Implicit RESTful Routing
+-------------------------------------------------------------------------------------------------*/
+Route::resource('tag', 'TagController');
+
+
+/*-------------------------------------------------------------------------------------------------
+# ! Book
+Explicit Routing
+-------------------------------------------------------------------------------------------------*/
+Route::get('/course', 'CourseController@getIndex');
+Route::get('/course/edit/{id}', 'CourseController@getEdit');
+Route::post('/course/edit/{id}', 'CourseController@postEdit');
+Route::get('/course/create', 'CourseController@getCreate');
+Route::post('/course/create', 'CourseController@postCreate');
+
+Route::get('/book/search', 'CourseController@getSearch');
+Route::post('/book/search', 'CourseController@postSearch');
+
+
+/*-------------------------------------------------------------------------------------------------
+// ! Debug
+Implicit
+-------------------------------------------------------------------------------------------------*/
+# Implicit routing
+Route::controller('debug', 'DebugController');
+
+/*
+# Explicit routing
+Route::get('/debug/', 'DebugController@index');
+Route::get('/debug/trigger-error', 'Debug Controller@triggerError');
+Route::get('/debug/books-json', 'DebugController@getBooksJson');
+Route::get('/debug/routes', 'DebugController@routes');
+*/
+
+
+/*-------------------------------------------------------------------------------------------------
+// ! Misc Demo
+Explicit Routing
+-------------------------------------------------------------------------------------------------*/
+Route::get('/demo/csrf-example', 'DemoController@csrf');
+Route::get('/demo/collections', 'DemoController@collections');
+Route::get('/demo/js-vars', 'DemoController@jsVars');
+
+
+/*-------------------------------------------------------------------------------------------------
+// ! CRUD Demo
+Explicit Routing
+-------------------------------------------------------------------------------------------------*/
+Route::get('/crud-create', 'DemoController@crudCreate');
+Route::get('/crud-read', 'DemoController@crudRead');
+Route::get('/crud-update', 'DemoController@crudUpdate');
+Route::get('/crud-delete', 'DemoController@crudDelete');
+
+
+/*-------------------------------------------------------------------------------------------------
+// ! Queries Demo
+Explicit Routing
+-------------------------------------------------------------------------------------------------*/
+Route::get('/collections', 'DemoController@collections');
+Route::get('/query-without-constraints', 'DemoController@queryWithoutConstraints');
+Route::get('/query-with-constraints', 'DemoController@queryWithConstraints');
+Route::get('/query-responsibility', 'DemoController@queryResponsibility');
+Route::get('/query-with-order', 'DemoController@queryWithOrder');
+
+
+/*-------------------------------------------------------------------------------------------------
+// ! Query Relationship Demos
+Explicit Routing
+-------------------------------------------------------------------------------------------------*/
+Route::get('/query-relationships-author', 'DemoController@queryRelationshipsAuthor');
+Route::get('/query-relationships-tags', 'DemoController@queryRelationshipstags');
+Route::get('/query-eager-loading-authors', 'DemoController@queryEagerLoadingAuthors');
+Route::get('/query-eager-loading-tags-and-authors', 'DemoController@queryEagerLoadingTagsAndAuthors');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 Route::get('/signup',
     array(
         'before' => 'guest',
@@ -27,8 +153,7 @@ Route::post('/signup',
             $user = new User;
             $user->email    = Input::get('email');
             $user->password = Hash::make(Input::get('password'));
-            $user->email    = Input::get('email');
-
+            
             # Try to add the user 
             try {
                 $user->save();
@@ -41,7 +166,7 @@ Route::post('/signup',
             # Log the user in
             Auth::login($user);
 
-            return Redirect::to('/list')->with('flash_message', 'Welcome to Foobooks!');
+            return Redirect::to('/list')->with('flash_message', 'Welcome to Harvard');
 
         }
     )
